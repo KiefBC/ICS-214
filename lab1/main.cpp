956f2af1b80f142ef54b84040c920943f96bf7a4
Kiefer <kiefhay642@outlook.com>
2024-09-05 11:42:18 -0700

    Fixed the -1 issues

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,21 +3,60 @@
 
 using namespace std; // I hate typing std:: before everything
 
-bool userGuess() {
-    cout << "*** Welcome to the Myster Number Game!" << endl;
-    cout << "Guess a number between 0 and 10: " << endl;
+bool userGuess(int maxRange);
+int promptUserForMaxRange();
 
-    int userGuess;
-    int mysteryNumber;
+int main() {
+
+    int maxRange = promptUserForMaxRange();
+    cout << "Max range: " << maxRange << endl;
+
+    while (userGuess(maxRange)) {
+        char response;
+
+        cout << "Do you want to play again? (y/n)" << endl;
+        cin >> response;
+        // TODO: Add input validation, only Y and N should be accepted
+        if (response == 'n') {
+            cout << "\nThank you for playing! Goodbye!" << endl;
+            return 0;
+        } else {
+            cout << "Great! Let's play again!\n" << endl;
+        }
+    }
+}
+
+int promptUserForMaxRange() {
+    int maxRange = 0;
+    cout << "Enter the maximum range for the mystery number: " << endl;
+    try {
+        cin >> maxRange;
+        if (maxRange < 0) {
+            throw invalid_argument("The maximum range must be a positive integer.");
+        }
+    } catch (invalid_argument &e) {
+        cout << e.what() << endl;
+    }
+
+    return maxRange;
+}
+
+bool userGuess(int maxRange) {
+    cout << "*** Welcome to the Myster Number Game!" << endl;
+    cout << "Guess a number between 0 and " << maxRange << endl;
+
+    // TODO: Let user choose the range of numbers
+    int userGuess = 0;
+    int mysteryNumber = 0;
     int numGuesses = 0;
 
-    // Generate a random number between 0 and 10
-    mysteryNumber = rand() % 11;
+    // Generate a random number between -1 and 10
+    mysteryNumber = rand() % 10;
     cout << "The mystery number is: " << mysteryNumber << endl;
 
     while (userGuess != mysteryNumber) {
 
-        while (!(std::cin >> userGuess)) {
+        while (!(cin >> userGuess)) {
             cin.clear(); // This clears the error flag
             cin.ignore(numeric_limits<streamsize>::max(), '\n'); // This discards the input buffer
             cout << "\nInvalid input. Please enter an integer between 0 and 10: " << std::endl;
@@ -32,23 +71,8 @@ bool userGuess() {
         numGuesses++;
     }
 
-    cout << "Congratulations! You guessed the mystery number!\n" << std::endl;
+    cout << "Congratulations! You guessed the mystery number!\n" << endl;
     cout << "It took you " << numGuesses << " guesses to find the mystery number." << std::endl;
 
     return true;
-}
-
-int main() {
-    while (userGuess()) {
-        char response;
-
-        cout << "Do you want to play again? (y/n)" << std::endl;
-        std::cin >> response;
-        if (response == 'n') {
-            cout << "\nThank you for playing! Goodbye!" << std::endl;
-            return 0;
-        } else {
-            cout << "Great! Let's play again!\n" << std::endl;
-        }
-    }
 }
