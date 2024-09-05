15d6961c34a5a8d7a22d3536cac7783bb7957dcb
Kiefer <kiefhay642@outlook.com>
2024-09-05 09:35:17 -0700

    Basic gameplay finished. Added namespace so I don't have to continue typing std::

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <limits>
+
+using namespace std; // I hate typing std:: before everything
+
+bool userGuess() {
+    cout << "*** Welcome to the Myster Number Game!" << endl;
+    cout << "Guess a number between 0 and 10: " << endl;
+
+    int userGuess;
+    int mysteryNumber;
+    int numGuesses = 0;
+
+    // Generate a random number between 0 and 10
+    mysteryNumber = rand() % 11;
+    cout << "The mystery number is: " << mysteryNumber << endl;
+
+    while (userGuess != mysteryNumber) {
+
+        while (!(std::cin >> userGuess)) {
+            cin.clear(); // This clears the error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // This discards the input buffer
+            cout << "\nInvalid input. Please enter an integer between 0 and 10: " << std::endl;
+        }
+
+        if (userGuess < mysteryNumber) {
+            cout << "\nYour guess is too low. Try again!" << endl;
+        } else if (userGuess > mysteryNumber) {
+            cout << "\nYour guess is too high. Try again!" << endl;
+        }
+
+        numGuesses++;
+    }
+
+    cout << "Congratulations! You guessed the mystery number!\n" << std::endl;
+    cout << "It took you " << numGuesses << " guesses to find the mystery number." << std::endl;
+
+    return true;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cin.get();
+    while (userGuess()) {
+        char response;
+
+        cout << "Do you want to play again? (y/n)" << std::endl;
+        std::cin >> response;
+        if (response == 'n') {
+            cout << "\nThank you for playing! Goodbye!" << std::endl;
+            return 0;
+        } else {
+            cout << "Great! Let's play again!\n" << std::endl;
+        }
+    }
 }
